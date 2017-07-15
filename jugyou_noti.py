faf87adfcea933ab078b078b8d869f4a4baf244b
stone-river <rikuisikawa11924@gmail.com>
2017-07-15 14:38:08 +0900

    Update jugyou_noti.py

--- jugyou_noti.py
+++ jugyou_noti.py
@@ -5,7 +5,7 @@ import requests
 #送信先URL（Line Notify）
 url     = "https://notify-api.line.me/api/notify"
 #送信用トークン
-token   = "Ar6en6ug1lb3aHKSXDlzSOgvq2GgfNUGyt2LHdARr5O"
+token   = "***"
 headers = {"Authorization" : "Bearer "+ token}
 
 message = "\n今日もお疲れ様でした。\n次授業日の持ち物・提出物・連絡事項などの記入に協力していただける方がいましたら、以下のURLで開かれるページのテキストを更新してください。(連絡事項がない場合は「なし」と記入していただけるとありがたいです)\npassword:0120\nhttps://writening.net/update?NVznd8"
